93d4f3bec934a828cff6d37d1426d379566d0566
roger <tuyideveloper@gmail.com>
2022-01-14 10:16:35 +0100

    handle error for divide method

--- divide.js
+++ divide.js
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports = function divide(a, b) {
-  return a / b;
+  const res = a / b;
+  if (Number.isNaN(res)) {
+    throw new Error("Invalid operation");
+  }
+  return res;
 };
