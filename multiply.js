f4c1ca5f5bfe13d5ab36301dd612bfc82b0861d9
roger <tuyideveloper@gmail.com>
2022-01-14 10:13:43 +0100

    handle error for multiply module

--- multiply.js
+++ multiply.js
@@ -1,5 +1,9 @@
 "use strict";
 
 module.exports = function multiply(a, b) {
-  return a * b;
+  const res = a * b;
+  if (Number.isNaN(res)) {
+    throw new Error("Invalid Operation");
+  }
+  return res;
 };
